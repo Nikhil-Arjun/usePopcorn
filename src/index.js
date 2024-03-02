f4e34d86991c9e01d2e77821f7697557d000a20b
Nikhil-Arjun <nikhilarjun97@gmail.com>
2024-03-02 19:57:31 +0530

    second commit

--- src/index.js
+++ src/index.js
@@ -9,7 +9,11 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
+    <StarRating
+      maxRating={5}
+      message={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
     {/* <StarRating maxRating={10} /> */}
+    <StarRating size={24} color="red" className="test" defaultRating={3} />
   </React.StrictMode>
 );
